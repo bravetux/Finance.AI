c6b341c44adae4d8eed4a65f91090a77caab4d68
[dyad] <jarvis@bravetux.com>
2025-08-14 22:23:10 +0530

    [dyad] Removed input boxes for current value - wrote 1 file(s)

--- src/pages/FutureValueCalculator.tsx
+++ src/pages/FutureValueCalculator.tsx
@@ -237,9 +237,7 @@ const FutureValueCalculator: React.FC = () => {
                 {data.map((asset) => (
                   <tr key={asset.name} className="h-10">
                     <td className="px-2 py-0 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">{asset.name}</td>
-                    <td className="px-2 py-0 whitespace-nowrap">
-                      <Input type="text" value={asset.currentValue.toLocaleString('en-IN')} className="w-36 h-7 text-sm" disabled />
-                    </td>
+                    <td className="px-2 py-0 whitespace-nowrap text-sm text-gray-900 dark:text-gray-100">{asset.currentValue.toLocaleString('en-IN')}</td>
                     <td className="px-2 py-0 whitespace-nowrap">
                       <Input type="number" value={asset.roi} onChange={(e) => handleInputChange(asset.name, 'roi', e.target.value)} className="w-16 h-7 text-sm" />
                     </td>
